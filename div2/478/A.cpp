8ae5eed0e51dd7c5f0b55a3e76a7473a278ec59b
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-05-06 22:17:08 +0900

    initial commit

---
+++ div2/478/A.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define int long long
+
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i < i##_len; ++i)
+#define REV(i, a) for (int i = (a); i >= 0; --i)
+#define CLR(a) memset((a), 0, sizeof(a))
+#define DUMP(x) cerr << #x << " = " << (x) << endl;
+#define INF (3e15)
+
+using namespace std;
+
+int N;
+string S[1005];
+
+signed main() {
+    cin >> N;
+    REP(i, N) cin >> S[i];
+
+    set<string> arset;
+    REP(i, N) {
+        string s = S[i];
+        sort(s.begin(), s.end());
+        s.erase(unique(s.begin(), s.end()), s.end());
+        arset.insert(s);
+    }
+    cout << arset.size() << endl;
+
+    return 0;
+}
