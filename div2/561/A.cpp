0b68f3c79f796ef3eef771ebddb9409bb2446ee9
tatsumack <tatsu.mack@gmail.com>
2019-05-18 11:10:20 +0900

    div2 561

---
+++ div2/561/A.cpp
@@ -0,0 +1,88 @@
+
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(10)
+
+using namespace std;
+
+template<typename T>
+vector<T> make_v(size_t a) { return vector<T>(a); }
+
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+    return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+    for (auto& e:t) fill_v(e, v);
+}
+
+typedef pair<int, int> P;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    map<char, int> cnt;
+    REP(i, n) {
+        string s;
+        cin >> s;
+        cnt[s[0]]++;
+    }
+
+    int res = 0;
+    for (auto& kv: cnt) {
+        int n = kv.second;
+        int t = kv.second / 2;
+        if (t > 1) {
+            res += t * (t - 1) / 2;
+        }
+        n -= t;
+        if (n > 1) {
+            res += n * (n - 1) / 2;
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
---
+++ div2/561/D.cpp
@@ -0,0 +1,94 @@
+
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(10)
+
+using namespace std;
+
+template<typename T>
+vector<T> make_v(size_t a) { return vector<T>(a); }
+
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+    return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+    for (auto& e:t) fill_v(e, v);
+}
+
+typedef pair<int, int> P;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int k;
+    cin >> k;
+    while (k--) {
+        int a, b, m;
+        cin >> a >> b >> m;
+
+        int min = a, max = a;
+        int smin = min, smax = max;
+        bool ok = false;
+        while (min <= b) {
+            min = smin + 1;
+            max = smax + m;
+            if (min <= b && b <= max) {
+                ok = true;
+                break;
+            }
+            smin += min;
+            smax += max;
+        }
+
+        if (ok) {
+            cout << 1 << endl;
+        }
+        else {
+            cout << -1 << endl;
+        }
+
+    }
+    return 0;
+}
