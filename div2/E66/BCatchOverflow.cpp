a348dab7f2fb68b81559333c4e14dce6628be0e7
tatsumack <tatsu.mack@gmail.com>
2019-06-07 00:32:24 +0900

    div2 E66

---
+++ div2/E66/BCatchOverflow.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+template<typename T>
+vector <T> make_v(size_t a) { return vector<T>(a); }
+
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+    return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+    for (auto& e:t) fill_v(e, v);
+}
+
+typedef pair<int, int> P;
+
+class BCatchOverflow {
+public:
+    static constexpr int kStressIterations = 0;
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int l;
+        cin >> l;
+        int res = 0;
+        stack<int> num;
+        stack<int> loop;
+        num.push(0);
+        while (l--) {
+            string s;
+            cin >> s;
+            if (s == "add") {
+                int n = num.top();
+                num.pop();
+                n++;
+                num.push(n);
+            }
+            if (s == "for") {
+                int cnt;
+                cin >> cnt;
+                loop.push(cnt);
+                num.push(0);
+            }
+            if (s == "end") {
+                int cnt = loop.top();
+                loop.pop();
+                int n = num.top();
+                num.pop();
+                int cur = num.top() + n * cnt;
+                num.pop();
+                num.push(cur);
+            }
+            if (num.top() >= (1LL << 32)) {
+                cout << "OVERFLOW!!!" << endl;
+                return;
+            }
+        }
+        cout << num.top() << endl;
+    }
+};
