5d70e920b9e478eac619fe6ab9cbd166fca8c791
tatsumack <tatsu.mack@gmail.com>
2019-10-12 12:09:12 +0900

    Add Div2 E74 C

---
+++ div2/E74/CStandardFree2play.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+class CStandardFree2play {
+public:
+    static constexpr int kStressIterations = 0;
+
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int q;
+        cin >> q;
+        while (q--) {
+            int h, n;
+            cin >> h >> n;
+            vector<int> p(n);
+            REP(i, n) cin >> p[i];
+
+            int cur = h;
+            int res = 0;
+            REP(i, n) {
+                if (cur <= p[i]) continue;
+                cur = p[i] + 1;
+                if ((i + 1 < n && p[i + 1] == cur - 2) || cur - 2 == 0) {
+
+                } else {
+                    res++;
+                }
+                cur -= 2;
+            }
+            cout << res << endl;
+        }
+    }
+};
